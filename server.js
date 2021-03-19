b291a793ef5bd529d65ce3cb60821641023162e9
byronmosley <mosleyb91@outlook.com>
2021-03-19 13:21:03 -0700

    changed main to be server.js instead of index.js in package.json

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'))
 
-  app.get('*', (_, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 }
